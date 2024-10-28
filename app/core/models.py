cfba676856b91c184a71a1ca97e450e8aa553d7c
harshita <hrs.dadhich@gmail.com>
2024-10-28 20:46:00 +0530

    mendatory email added

--- app/core/models.py
+++ app/core/models.py
@@ -15,6 +15,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """create, save and return a new user"""
+        if not email:
+            raise ValueError(" user must have an email")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)

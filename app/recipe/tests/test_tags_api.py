1f1883ecb5b8257c688c520988178e71f68457f9
harshita <hrs.dadhich@gmail.com>
2024-11-20 13:36:28 +0530

    delete tags

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -83,3 +83,15 @@ class PrivateTagAPI(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         tags.refresh_from_db()
         self.assertEqual(tags.name, payload["name"])
+
+    def test_delete_tag(self):
+        """test delete tag"""
+
+        tag = Tag.objects.create(user=self.user, name="Breakfast")
+
+        urls = detail_url(tag.id)
+        res = self.client.delete(urls)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -31,7 +31,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
 
 class TagViewSet(
-    mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
 ):
     """View to return tags"""
 

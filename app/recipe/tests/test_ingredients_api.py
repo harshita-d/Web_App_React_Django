36b13ca12809ae9d937621807a44fb9f33c8d2c8
harshita <hrs.dadhich@gmail.com>
2024-11-28 14:49:21 +0530

    linting issue fix

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -96,5 +96,3 @@ class privateIngredientsAPI(TestCase):
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         ing = Ingredient.objects.filter(user=self.user)
         self.assertFalse(ing.exists())
-
-
